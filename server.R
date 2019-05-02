5994117c46cff695ad4bc07fae11a609f69667af
Guneev Kaur <kaurguneev@gmail.com>
2019-05-02 04:24:32 -0500

    Update server.R

--- server.R
+++ server.R
@@ -77,14 +77,92 @@ server <- function(input, output)
             sidebarMenu()}
   })
   
+  output$plot10 <- renderPlot({
+    
+    data %>%
+      group_by(year) %>%
+      summarize(population = sum(population), 
+                suicides = sum(`suicide-no`), 
+                suicides100kpop = (suicides / population) * 100000) %>%
+      ggplot(aes(x = year, y = suicides100kpop)) +
+      geom_line(col = "deepskyblue3", size = 1) +
+      geom_point(col = "deepskyblue3", size = 2) +
+      labs(
+        title = "Global Suicides per 100k",
+        subtitle = "Trend over time, 1985 - 2015.",
+        x = "Year",
+        y = "Suicides per 100k") +
+      scale_x_continuous(breaks = seq(1985, 2015, 2)) + 
+      scale_y_continuous(breaks = seq(10, 20))
+        
+  })
+  
+  output$plot11 <- renderPlot({
+    
+    data %>%
+      group_by(sex) %>%
+      summarize(suicides100kpop = (sum(as.numeric(`suicide-no`)) / sum(as.numeric(population))) * 100000) %>%
+      ggplot(aes(x = sex, y = suicides100kpop, fill = sex)) + 
+      geom_bar(stat = "identity") + 
+      labs(title = "Global suicides (per 100k), by Sex",
+           x = "Sex", 
+           y = "Suicides per 100k")
+  })  
+  
+  output$plot12 <- renderPlot({
+    
+      data %>%
+      group_by(year, sex) %>%
+      summarize(suicides100kpop = (sum(as.numeric(`suicide-no`)) / sum(as.numeric(population))) * 100000) %>%
+      ggplot(aes(x = year, y = suicides100kpop, col = factor(sex))) + 
+      facet_grid(sex ~ ., scales = "free_y") + 
+      geom_line() + 
+      geom_point() + 
+      labs(title = "Trends Over Time, by Sex", 
+           x = "Year", 
+           y = "Suicides per 100k", 
+           color = "Sex")
+    
+  })
+  
+  output$plot13<-renderPlot({
+  
+    data %>%
+      group_by(age) %>%
+      summarize(suicides100kpop = (sum(as.numeric(`suicide-no`)) / sum(as.numeric(population))) * 100000) %>%
+      ggplot(aes(x = age, y = suicides100kpop, fill = age)) + 
+      geom_bar(stat = "identity") + 
+      labs(title = "Global suicides per 100k, by Age",
+           x = "Age", 
+           y = "Suicides per 100k") 
+    
+  })
+  
+  
+  output$plot14 <- renderPlot({
+    
+    data %>%
+      group_by(year, age) %>%
+      summarize(suicides100kpop = (sum(as.numeric(`suicide-no`)) / sum(as.numeric(population))) * 100000) %>%
+      ggplot(aes(x = year, y = suicides100kpop, col = age)) + 
+      facet_grid(age ~ ., scales = "free_y") + 
+      geom_line() + 
+      geom_point() + 
+      labs(title = "Trends Over Time, by Age", 
+           x = "Year", 
+           y = "Suicides per 100k", 
+           color = "Age")
+  })
+    
   output$plot1 <- renderPlot({
+    
     globalsuicide %>%
       filter(country == input$Country) %>%
       ggplot(aes(x = year, y = suicides100kpop)) +
       geom_line(col = "deepskyblue3", size = 1) +
       geom_point(col = "deepskyblue3", size = 2) +
       labs(
-        title = "Global Suicides (per 100k)",
+        title = paste("Global Suicides (per 100k) in ",input$Country,sep=" "),
         subtitle = "Trend over time, 1985 - 2015.",
         x = "Year",
         y = "Suicides per 100k"
